1dba03e5c840e4abd84da68debc8a0597b8e36f4
Park HanGyeol <gksruf0025@gmail.com>
2023-01-15 19:06:22 +0900

    Add dsigmoid function

--- functions.py
+++ functions.py
@@ -9,3 +9,7 @@ class function:
         exp_x = np.exp(x - np.max(x))
 
         return  exp_x / np.sum(exp_x, axis=0)
+
+    def dsigmoid(x):
+        s = 1 / (1 + np.exp(-x))
+        return s * (1 - s)
